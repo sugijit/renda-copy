3fed9db8daa4c029443f6b06ea98e3752d43d236
sugijit <b.suldsaikhan@gmail.com>
2023-08-13 22:14:46 +0900

    eventlistener追加した

---
+++ app.js
@@ -0,0 +1,28 @@
+document.querySelector(".btn-start1").addEventListener("click", function () {
+  console.log("clicked");
+});
+document.querySelector(".btn-start2").addEventListener("click", function () {
+  console.log("clicked");
+});
+
+
+
+
+var click = function {
+     if 
+}
+
+document.querySelector(".player-1-1").addEventListener("click", function () {
+  console.log("left clicked");
+});
+
+document.querySelector(".player-1-2").addEventListener("click", function () {
+  console.log("left clicked");
+});
+document.querySelector(".player-2-1").addEventListener("click", function () {
+  console.log("right clicked");
+});
+
+document.querySelector(".player-2-2").addEventListener("click", function () {
+  console.log("right clicked");
+});
