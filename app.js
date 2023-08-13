4b5221ffb7e7aa6752636cfc4989231cf45c61b2
sugijit <b.suldsaikhan@gmail.com>
2023-08-13 23:27:13 +0900

    click function done

--- app.js
+++ app.js
@@ -1,21 +1,33 @@
-document.querySelector(".btn-start1").addEventListener("click", function () {
-  console.log("clicked");
-});
-document.querySelector(".btn-start2").addEventListener("click", function () {
-  console.log("clicked");
+var score1 = 0;
+var score2 = 0;
+
+const start1 = document.getElementById(".btn-start1"); //button left
+const start2 = document.getElementById(".btn-start2"); //button left
+const leftButtons = document.querySelectorAll(".player-1"); //button left
+const rightButtons = document.querySelectorAll(".player-2"); //button left
+const pointP1 = document.querySelectorAll(".point-p1"); //point
+const pointP2 = document.querySelectorAll(".point-p2"); //point
+const P1 = document.getElementById("1");
+const P2 = document.getElementById("2");
+
+leftButtons.forEach((button) => {
+  button.addEventListener("click", function () {
+    score1++;
+    P1.textContent = score1;
+  });
 });
 
-document.querySelector(".player-1-1").addEventListener("click", function () {
-  console.log("left clicked");
+rightButtons.forEach((button) => {
+  button.addEventListener("click", function () {
+    score2++;
+    P2.textContent = score2;
+  });
 });
 
-document.querySelector(".player-1-2").addEventListener("click", function () {
-  console.log("left clicked");
-});
-document.querySelector(".player-2-1").addEventListener("click", function () {
-  console.log("right clicked");
-});
+function init() {
+  P1.textContent = 0;
+  P2.textContent = 0;
+  start1.textContent = "OMG";
+}
 
-document.querySelector(".player-2-2").addEventListener("click", function () {
-  console.log("right clicked");
-});
+function timer() {}
