7c757d875737bfc5fdb9284c655739100ebbea25
sugijit <b.suldsaikhan@gmail.com>
2023-08-29 22:35:18 +0900

    GAME DONE BUT MULTI-TOUCH!!!!!

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-var score1 = 0;
-var score2 = 0;
 var intervalId;
 
 //DOM
@@ -15,6 +13,7 @@ const rightTop = document.getElementById("btnn1"); //button right
 const rightBottom = document.getElementById("btnn2"); //button right
 const leftTop = document.getElementById("btnn3"); //button left
 const leftBottom = document.getElementById("btnn4"); //button left
+const refresh = document.querySelector(".refresh"); //refresh
 
 start1.addEventListener("click", function () {
   countDown3();
@@ -22,25 +21,37 @@ start1.addEventListener("click", function () {
 start2.addEventListener("click", function () {
   countDown3();
 });
-
-// function start() {}
+refresh.addEventListener("click", function () {
+  init();
+  refresh.classList.add("invisible");
+});
 
 //Ehluuleh function
 function init() {
   P1.textContent = 0;
   P2.textContent = 0;
   start1.textContent = "START";
+  start2.textContent = "START";
+  start1.classList.remove("btn-dead");
+  start2.classList.remove("btn-dead");
+  refresh.classList.remove("invisible");
 }
 
 // 10sec
 function countDown10() {
   let timeLeft = 10;
+  start1.classList.add("btn-dead");
+  start2.classList.add("btn-dead");
 
   function updateCountdown() {
     if (timeLeft > 0) {
       start1.textContent = timeLeft.toFixed(1);
       start2.textContent = timeLeft.toFixed(1);
       timeLeft = timeLeft - 0.1;
+      rightTop.classList.remove("btn-dead");
+      rightBottom.classList.remove("btn-dead");
+      leftTop.classList.remove("btn-dead");
+      leftBottom.classList.remove("btn-dead");
       return timeLeft;
     } else {
       clearInterval(intervalId);
@@ -49,7 +60,10 @@ function countDown10() {
       rightBottom.classList.add("btn-dead");
       leftTop.classList.add("btn-dead");
       leftBottom.classList.add("btn-dead");
+      start1.classList.add("btn-dead");
+      start2.classList.add("btn-dead");
       decideWinner();
+      refresh.classList.remove("invisible");
     }
   }
   if (timeLeft > 0) {
@@ -59,22 +73,12 @@ function countDown10() {
   intervalId = setInterval(updateCountdown, 100);
 }
 
-function decideWinner() {
-  if (P1.textContent === P2.textContent) {
-    start1.textContent = "DRAW";
-    start2.textContent = "DRAW";
-  } else if (P1.textContent > P2.textContent) {
-    start1.textContent = "WINNER";
-    start2.textContent = "LOSER!";
-  } else {
-    start1.textContent = "LOSER!";
-    start2.textContent = "WINNER";
-  }
-}
-
 // 3sec
 function countDown3() {
   let count = 3;
+  start1.classList.add("btn-dead");
+  start2.classList.add("btn-dead");
+  refresh.classList.add("invisible");
 
   function updateCountdown() {
     if (count > 0) {
@@ -92,8 +96,11 @@ function countDown3() {
   intervalId = setInterval(updateCountdown, 1000);
 }
 
+// click
 function click(timeLeft) {
   if (timeLeft > 0) {
+    var score1 = 0;
+    var score2 = 0;
     leftButtons.forEach((button) => {
       button.addEventListener("click", function () {
         score1++;
@@ -112,6 +119,16 @@ function click(timeLeft) {
   }
 }
 
-// countDown3();
-
-//countDown10();
+// who is winner
+function decideWinner() {
+  if (P1.textContent === P2.textContent) {
+    start1.textContent = "DRAW";
+    start2.textContent = "DRAW";
+  } else if (P1.textContent > P2.textContent) {
+    start1.textContent = "WINNER";
+    start2.textContent = "LOSER!";
+  } else {
+    start1.textContent = "LOSER!";
+    start2.textContent = "WINNER";
+  }
+}
